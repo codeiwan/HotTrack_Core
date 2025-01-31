3e941d48a3f873ee72a062a7c83a1e9ab24e194c
codeiwan <pewlv7976@gmail.com>
2025-02-01 04:48:33 +0900

    feat: 메시지 저장 및 활용 - View를 통해 저장, 템플릿을 통해 메시지 소비 - Debug 메시지 처리 기능 추가 - bootstrap5 alert 스타일 적용

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
+from django.contrib import messages
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    messages.debug(request, message="디버그 메시지")
+    messages.info(request, message="정보 메시지")
+    messages.success(request, message="성공 메시지")
+    messages.warning(request, message="경고 메시지")
+    messages.error(request, message="에러 메시지")
+    
+    return render(request, template_name="core/index.html")
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# messages framework
+
+from django.contrib.messages import constants as messages_constants
+
+if DEBUG:
+    MESSAGE_LEVEL = messages_constants.DEBUG
